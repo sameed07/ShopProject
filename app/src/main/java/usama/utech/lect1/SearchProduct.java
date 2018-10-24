a64256de920cad24c3c95cef6940e4c543d706cd
Sameed Shah <sameedshah8@gmail.com>
2018-10-24 22:54:53 +0500

    Searchbar modified!

--- app/src/main/java/usama/utech/lect1/SearchProduct.java
+++ app/src/main/java/usama/utech/lect1/SearchProduct.java
@@ -4,12 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import usama.utech.lect1.Adapter.ProductAdapter;
