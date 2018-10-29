1f5057ccb2a925de20987f71eb1a6bf49e564f8f
Syed Usama Ahmad <syedusama5556>
2018-10-29 22:06:28 +0500

    Added Models Of All Tables And Updated ProductDB.java

--- app/src/main/java/usama/utech/lect1/Database/ProductDb.java
+++ app/src/main/java/usama/utech/lect1/Database/ProductDb.java
@@ -1,5 +1,6 @@
 package usama.utech.lect1.Database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,7 +10,12 @@ import android.database.sqlite.SQLiteQueryBuilder;
 import java.util.ArrayList;
 import java.util.List;
 
+import usama.utech.lect1.Model.Buyer;
+import usama.utech.lect1.Model.Expense;
 import usama.utech.lect1.Model.Products;
+import usama.utech.lect1.Model.Profit;
+import usama.utech.lect1.Model.Sale;
+import usama.utech.lect1.Model.Users;
 
 public class ProductDb  extends SQLiteOpenHelper{
 
@@ -28,22 +34,14 @@ public class ProductDb  extends SQLiteOpenHelper{
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
 
-        String productTableCreateQuery = "CREATE TABLE "+TABLE_PRODUCTS+" (Id INTEGER ,BarCode TEXT, Name TEXT,Quantity TEXT" +
-        ",SalePrice TEXT ,PurchasePrice TEXT,WholeSalePrice TEXT) ";
-        String userTableCreateQuery = "CREATE TABLE "+TABLE_USERS+" (Id INTEGER PRIMARY KEY, Username TEXT, Password TEXT )";
-        String buyerTableCreateQuery = "CREATE TABLE "+TABLE_BUYER+" (Id INTEGER PRIMARY KEY , BuyerName text, TotalAmount text, PaidAmount text, RemainingAmount text, LastPaidDate text)";
-        String profitTableCreateQuery = "CREATE TABLE "+TABLE_PROFIT+" (Id INTEGER PRIMARY KEY, \"Date\" text, \"Time\" text, Profit text)";
-        String saleTableCreateQuery = "CREATE TABLE "+TABLE_SALE+" (Id INTEGER PRIMARY KEY,CustomerName text, ProductName text, QuantitySold text, \"Date\" text, \"Time\" text)";
-        String expenseTableCreateQuery = "CREATE TABLE "+TABLE_EXPENSE+" (Id INTEGER PRIMARY KEY,ItemName text,AmountPaid text, \"Date\" text, \"Time\" text)" ;
-
-        String allQuerys[] = {productTableCreateQuery, userTableCreateQuery, buyerTableCreateQuery, profitTableCreateQuery, saleTableCreateQuery,  expenseTableCreateQuery};
-
-        //to test
-        for (String querys: allQuerys) {
-            sqLiteDatabase.execSQL(querys);
-        }
+        db.execSQL("CREATE TABLE "+TABLE_PRODUCTS+" (Id INTEGER ,BarCode TEXT, Name TEXT,Quantity TEXT" + ",SalePrice TEXT ,PurchasePrice TEXT,WholeSalePrice TEXT) ");
+        db.execSQL("CREATE TABLE "+TABLE_USERS+" (Id INTEGER PRIMARY KEY, Username TEXT, Password TEXT )");
+        db.execSQL("CREATE TABLE "+TABLE_BUYER+" (Id INTEGER PRIMARY KEY , BuyerName text, TotalAmount text, PaidAmount text, RemainingAmount text, LastPaidDate text)");
+        db.execSQL("CREATE TABLE "+TABLE_PROFIT+" (Id INTEGER PRIMARY KEY, \"Date\" text, \"Time\" text, Profit text)");
+        db.execSQL("CREATE TABLE "+TABLE_SALE+" (Id INTEGER PRIMARY KEY,CustomerName text, ProductName text, QuantitySold text, \"Date\" text, \"Time\" text)");
+        db.execSQL("CREATE TABLE "+TABLE_EXPENSE+" (Id INTEGER PRIMARY KEY,ItemName text,AmountPaid text, \"Date\" text, \"Time\" text)" );
 
 
     }
@@ -109,4 +107,233 @@ public class ProductDb  extends SQLiteOpenHelper{
 
         return result;
     }
+
+
+    //used to get all data from tables
+
+    public ArrayList<Users> getAllUserTableData(){
+
+        ArrayList<Users> resultCard = new ArrayList<Users>();
+
+        String selectDataQuery = "select * from "+TABLE_USERS;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectDataQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Users content = new Users();
+                content.setId(Integer.parseInt(cursor.getString(0)));
+                content.setUsername(cursor.getString(1));
+                content.setPassword(cursor.getString(2));
+                // Adding Data to list
+                resultCard.add(content);
+            } while (cursor.moveToNext());
+        }
+
+        db.close(); // Closing database connection
+        return resultCard;
+    }
+
+    public ArrayList<Profit> getAllProfitTableData(){
+
+        ArrayList<Profit> resultCard = new ArrayList<Profit>();
+
+        String selectDataQuery = "select * from "+TABLE_PROFIT;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectDataQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Profit content = new Profit();
+                content.setId(Integer.parseInt(cursor.getString(0)));
+                content.setDate(cursor.getString(1));
+                content.setTime(cursor.getString(2));
+                content.setProfit(Double.parseDouble(cursor.getString(3)));
+                // Adding Data to list
+                resultCard.add(content);
+            } while (cursor.moveToNext());
+        }
+
+        db.close(); // Closing database connection
+        return resultCard;
+    }
+
+    public ArrayList<Sale> getAllSaleTableData(){
+
+        ArrayList<Sale> resultCard = new ArrayList<Sale>();
+
+        String selectDataQuery = "select * from "+TABLE_SALE;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectDataQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Sale content = new Sale();
+                content.setId(Integer.parseInt(cursor.getString(0)));
+                content.setCustomerName(cursor.getString(1));
+                content.setProductName(cursor.getString(2));
+                content.setQuantitySold(Integer.parseInt(cursor.getString(3)));
+                content.setDate(cursor.getString(4));
+                content.setTime(cursor.getString(5));
+                // Adding Data to list
+                resultCard.add(content);
+            } while (cursor.moveToNext());
+        }
+
+        db.close(); // Closing database connection
+        return resultCard;
+    }
+
+    public ArrayList<Expense> getAllExpenseTableData(){
+
+        ArrayList<Expense> resultCard = new ArrayList<Expense>();
+
+        String selectDataQuery = "select * from "+TABLE_EXPENSE;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectDataQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Expense content = new Expense();
+                content.setId(Integer.parseInt(cursor.getString(0)));
+                content.setItemName(cursor.getString(1));
+                content.setAmountPaid(Double.parseDouble(cursor.getString(2)));
+                content.setDate(cursor.getString(3));
+                content.setTime(cursor.getString(4));
+                // Adding Data to list
+                resultCard.add(content);
+            } while (cursor.moveToNext());
+        }
+
+        db.close(); // Closing database connection
+        return resultCard;
+    }
+
+    public ArrayList<Buyer> getAllBuyerTableData(){
+
+        ArrayList<Buyer> resultCard = new ArrayList<Buyer>();
+
+        String selectDataQuery = "select * from "+TABLE_BUYER;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectDataQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                Buyer content = new Buyer();
+                content.setId(Integer.parseInt(cursor.getString(0)));
+                content.setBuyerName(cursor.getString(1));
+                content.setTotalAmount(Double.parseDouble(cursor.getString(2)));
+                content.setPaidAmount(Double.parseDouble(cursor.getString(3)));
+                content.setRemainingAmount(Double.parseDouble(cursor.getString(4)));
+                content.setLastPaidDate(cursor.getString(5));
+                // Adding Data to list
+                resultCard.add(content);
+            } while (cursor.moveToNext());
+        }
+
+        db.close(); // Closing database connection
+        return resultCard;
+    }
+
+
+
+    //All tables insert quserys
+
+    public void insertInToUserTable(Users users){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("Username", users.getUsername());
+        values.put("Password", users.getPassword());
+
+        // Inserting Row
+        db.insert(TABLE_USERS, null, values);
+        //2nd argument is String containing nullColumnHack
+        db.close(); // Closing database connection
+
+    }
+
+    public void insertInToProfitTable(Profit profit){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("Date", profit.getDate());
+        values.put("Time", profit.getTime());
+        values.put("Profit", profit.getProfit());
+
+        // Inserting Row
+        db.insert(TABLE_PROFIT, null, values);
+        //2nd argument is String containing nullColumnHack
+        db.close(); // Closing database connection
+
+    }
+
+    public void insertInToSaleTable(Sale sale){
+
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("CustomerName", sale.getCustomerName());
+        values.put("ProductName", sale.getProductName());
+        values.put("QuantitySold", sale.getQuantitySold());
+        values.put("Date", sale.getDate());
+        values.put("Time", sale.getTime());
+
+        // Inserting Row
+        db.insert(TABLE_SALE, null, values);
+        //2nd argument is String containing nullColumnHack
+        db.close(); // Closing database connection
+
+    }
+
+    public void insertInToExpenseTable(Expense expense){
+
+
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("ItemName", expense.getItemName());
+        values.put("AmountPaid", expense.getAmountPaid());
+        values.put("Date", expense.getDate());
+        values.put("Time", expense.getTime());
+
+        // Inserting Row
+        db.insert(TABLE_EXPENSE, null, values);
+        //2nd argument is String containing nullColumnHack
+        db.close(); // Closing database connection
+
+    }
+
+    public void insertInToBuyerTable(Buyer buyer){
+
+
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put("BuyerName", buyer.getBuyerName());
+        values.put("TotalAmount", buyer.getTotalAmount());
+        values.put("PaidAmount", buyer.getPaidAmount());
+        values.put("RemainingAmount", buyer.getRemainingAmount());
+        values.put("LastPaidDate", buyer.getLastPaidDate());
+
+        // Inserting Row
+        db.insert(TABLE_BUYER, null, values);
+        //2nd argument is String containing nullColumnHack
+        db.close(); // Closing database connection
+
+    }
+
+
+
 }
