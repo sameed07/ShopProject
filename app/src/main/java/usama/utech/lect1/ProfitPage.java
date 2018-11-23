c8ebc0af3264aca1fed25856c08520b1a8546f9d
Sameed Shah <sameedshah8@gmail.com>
2018-11-23 23:08:41 +0500

    Added graphviews

--- app/src/main/java/usama/utech/lect1/ProfitPage.java
+++ app/src/main/java/usama/utech/lect1/ProfitPage.java
@@ -2,13 +2,20 @@ package usama.utech.lect1;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.Series;
 
 import usama.utech.lect1.Database.ProductDb;
 
@@ -26,10 +33,47 @@ public class ProfitPage extends AppCompatActivity {
         db = new ProductDb(this);
         sqLiteDatabase = db.getWritableDatabase();
         graph = findViewById(R.id.graph);
-        graph.setTitle("Profit");
+        GraphView graph2 = (GraphView) findViewById(R.id.graph2);
+
+        graph.setTitle("Profit (Line Graph)");
+        graph2.setTitle("Profit (Bar Graph)");
         series = new LineGraphSeries<DataPoint>(getData());
         graph.addSeries(series);
-        Toast.makeText(this, ""+ getData(), Toast.LENGTH_SHORT).show();
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setHorizontalLabels(new String[] {"nov","dec","jan","feb"});
+        staticLabelsFormatter.setVerticalLabels(new String[] {"500","1000","1500","2000"});
+        //staticLabelsFormatter.setVerticalLabels(new String[] {&quot;Sun&quot;, &quot;Mon&quot;, &quot;Tue&quot;});
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+        BarGraphSeries<DataPoint> series2 = new BarGraphSeries<DataPoint>(getData());
+        graph2.addSeries(series2);
+        StaticLabelsFormatter staticLabelsFormatter2 = new StaticLabelsFormatter(graph2);
+        staticLabelsFormatter2.setHorizontalLabels(new String[] {"nov","dec","jan","feb"});
+        staticLabelsFormatter2.setVerticalLabels(new String[] {"500","1000","1500","2000"});
+        //staticLabelsFormatter.setVerticalLabels(new String[] {&quot;Sun&quot;, &quot;Mon&quot;, &quot;Tue&quot;});
+        graph2.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter2);
+
+        series2.setValueDependentColor(new ValueDependentColor() {
+            @Override
+            public int get(DataPointInterface data) {
+                return Color.rgb((int) data.getX() * 255 / 4, (int) Math.abs(data.getY() * 255 / 6), 100);
+
+            }
+        });
+        series2.setSpacing(20);
+        // draw values on top
+        series2.setDrawValuesOnTop(true);
+        series2.setValuesOnTopColor(Color.RED);
+        series2.setValuesOnTopSize(40);
+        // tap listener
+        series2.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(ProfitPage.this, "datapoint is "+dataPoint, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
     private DataPoint[] getData() {
